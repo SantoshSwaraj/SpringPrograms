834fabca7bbcf26cdcd79c6d625027375e40d76d
SantoshSwaraj <skushwaha.mca.bbau@gmail.com>
2017-04-17 15:37:24 +0530

    DI xml basesd

---
+++ DI_XMLBased_byName/src/main/model/Employee.java
@@ -0,0 +1,44 @@
+package main.model;
+
+public class Employee {
+	private Integer empId;
+	private String empName;
+	private Address address;
+	/**
+	 * @return the empId
+	 */
+	public Integer getEmpId() {
+		return empId;
+	}
+	/**
+	 * @param empId the empId to set
+	 */
+	public void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
+	/**
+	 * @return the empName
+	 */
+	public String getEmpName() {
+		return empName;
+	}
+	/**
+	 * @param empName the empName to set
+	 */
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	/**
+	 * @return the address
+	 */
+	public Address getAddress() {
+		return address;
+	}
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+}
---
+++ DI_XMLBased_constructor/src/main/model/Employee.java
@@ -0,0 +1,54 @@
+package main.model;
+
+import main.model.Address;
+
+public class Employee {
+	private Integer empId;
+	private String empName;
+	private Address address;
+	
+	public Employee(Integer empId, String empName, Address address) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+		this.address = address;
+	}
+	/**
+	 * @return the empId
+	 */
+	public Integer getEmpId() {
+		return empId;
+	}
+	/**
+	 * @param empId the empId to set
+	 */
+	public void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
+	/**
+	 * @return the empName
+	 */
+	public String getEmpName() {
+		return empName;
+	}
+	/**
+	 * @param empName the empName to set
+	 */
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	/**
+	 * @return the address
+	 */
+	public Address getAddress() {
+		return address;
+	}
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+}
+
